c9f989902ac3b2a68c512ef580eff528882cfb1d
Eswar Divi <76403422+EswarDivi@users.noreply.github.com>
2023-03-04 20:12:42 +0000

    new things

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import './App.css';
+import "./App.css";
+
+function openInColab() {
+  // get the notebook link from the input box
+  const notebookLink = document.getElementById("notebookLink").value;
+  // create a new window to open the notebook in colab
+  if (notebookLink) {
+
+    if(notebookLink.split("github.com/").length === 1 || !notebookLink.endsWith(".ipynb")) {
+      alert("Please enter a valid github link")
+      return;
+    }
+    window.open(
+      "https://colab.research.google.com/github/" +
+        notebookLink.split("github.com/")[1]
+    );
+  }
+}
+
+function openInKaggle() {
+  // get the notebook link from the input box
+  const notebookLink = document.getElementById("notebookLink").value;
+  if (notebookLink) {
+
+    // check if the link is a github link and ends with .ipynb
+    if(notebookLink.split("github.com/").length === 1 || !notebookLink.endsWith(".ipynb")) {
+      alert("Please enter a valid github link")
+      return;
+    }
+
+
+    window.open("https://kaggle.com/kernels/welcome?src=" + notebookLink);
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+        <h1>Open in Colab or Kaggle</h1>
+
+        <br />
+
+        <p>Enter the link to your notebook on GitHub</p>
+
+        <br />
+        <div className="AppMain">
+          <input
+            type="text"
+            id="notebookLink"
+            placeholder="Enter Notebook Link"
+          />
+
+          <br />
+
+          <button onClick={openInColab}>Open in Colab</button>
+          <br />
+          <button onClick={openInKaggle}>Open in Kaggle</button>
+        </div>
       </header>
     </div>
   );
